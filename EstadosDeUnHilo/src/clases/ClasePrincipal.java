98308978c8f93d9f98a541bba8d13e9b84e3a84a
R7Rivera <56278637+R7Rivera@users.noreply.github.com>
2022-04-27 22:29:22 -0500

    Add files via upload

---
+++ EstadosDeUnHilo/src/clases/ClasePrincipal.java
@@ -0,0 +1,26 @@
+package clases;
+
+public class ClasePrincipal {
+    public static void main(String[] args) {
+        
+        Hilo_Proceso hilo1 = new Hilo_Proceso();
+        Hilo_Proceso hilo2 = new Hilo_Proceso();
+        
+        hilo1.start();
+        
+        try{
+        hilo1.sleep(1000);
+        }catch(InterruptedException e){
+            System.out.println("Error en el hilo1 " + e);
+        }
+        
+        hilo2.start();
+        hilo2.stop();
+        
+        try{
+        hilo2.sleep(1000);
+        } catch (InterruptedException e) {
+            System.out.println("Error en el hilo 2 " + e);
+        }
+    }
+}
